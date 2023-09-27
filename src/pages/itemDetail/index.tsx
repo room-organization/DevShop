61d90c835af74e72aba6dbd474fa5bded473f1e1
enoquetembe <enoquetembetembe10@gmail.com>
2023-09-27 03:05:33 +0200

    feat: adding product screen

--- src/pages/itemDetail/index.tsx
+++ src/pages/itemDetail/index.tsx
@@ -1,33 +1,49 @@
 import { View, Text, Image, Button, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
-import {styles} from './styles';
+import { styles } from "./styles";
 import theme from "../../global/styles/theme";
+import { ScrollView } from "react-native-gesture-handler";
 
-
-
-export const ItemDetail=()=> {
-
-  
+export const ItemDetail = () => {
   return (
-      <TouchableOpacity >        
-        <View style={styles.card}>
-          <Image
-            source={require('../../../assets/adaptive-icon.png')}
-            style={styles.image}
-          />
-          <View style={styles.content}>
-            <Text style={styles.title}>Title</Text>
-            <Text style={styles.price}>R$ Price</Text>
-            <Text style={styles.description}>Description</Text>
-            <TouchableOpacity>
-               <Button 
-                // onPress={() => this.addProductToCart(product)}
-                title="Adicionar ao carrinho"
-                color={theme.colors_backgrounds.secondary}/>
-
-              </TouchableOpacity>
-            </View>
+    <View style={styles.container}>
+      <View style={styles.showcase}>
+        <View style={styles.icon}>
+          <Feather name="arrow-left" size={32} />
         </View>
-      </TouchableOpacity>
-    );
+
+        <Image
+          source={require("../../assets/images/T_Shirt.png")}
+          style={styles.image}
+        />
+      </View>
+
+      <ScrollView>
+        <View style={styles.details}>
+          <Text style={styles.title}>Camiseta 1</Text>
+          <Text style={styles.label}>Product details</Text>
+          <Text style={styles.description}>
+            Aliquam elementum molestie pretium. Vestibulum congue ornare
+            convallis. Mauris laoreet placerat tellus, quis rhoncus mauris
+            dapibus vitae. Vestibulum at pellentesque sapien. Quisque rutrum
+            porttitor nibh vel congue.
+          </Text>
+        </View>
+      </ScrollView>
+
+      <View style={styles.paymentContainer}>
+        <View style={styles.priceContainer}>
+          <Text style={styles.priceText}>Total Price</Text>
+          <Text style={styles.price}>$18,00</Text>
+        </View>
+
+        <View>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.textButton}>Add to card</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
 };
--- src/pages/itemDetail/styles.ts
+++ src/pages/itemDetail/styles.ts
@@ -1,38 +1,96 @@
 import { StyleSheet } from "react-native";
+import theme from "../../global/styles/theme";
 
 
 export const styles = StyleSheet.create({
-    card: {
-      backgroundColor: '#ffffff',
-      borderRadius: 10,
+    container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+    }, 
+
+    showcase: {
+      backgroundColor: '#D9D9D999',
       shadowColor: '#000000',
-      shadowOffset: {
-        width: 2,
-        height: 2,
-      },
-      shadowOpacity: 0.2,
-      shadowRadius: 10,
+      padding: 5
     },
+
+    icon: {
+      marginLeft: "2%",
+      marginTop: "4%",
+      backgroundColor: "#FFF",
+      width: 48,
+      height: 48,
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 24,
+    },
+
     image: {
       width: '100%',
       height: 200,
       resizeMode: 'contain',
+    }, 
+
+    details: {
+      color: "#00000",
+      padding: 30,
     },
-    content: {
-      padding: 10,
-      flex: 1,
-    },
+
     title: {
-      fontSize: 18,
+      fontSize: 24,
+      paddingBottom: 5,
       fontWeight: 'bold',
     },
-    price: {
+
+    label: {
+      color: "#00000",
+      fontWeight: "bold",
+      fontSize: 18,
+      paddingBottom: 20,
+    },
+
+    paymentContainer: {
+      backgroundColor: "#D9D9D999",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      paddingHorizontal: 30,
+      padding:10,
+      alignItems: "center",
+    },
+
+    priceContainer: {
+      gap: 2,
+    }, 
+    
+    button: {
+      backgroundColor: "#3C5A5D",
+      fontWeight: "bold",
       fontSize: 16,
+      height: 30,
+      paddingVertical: 16,
+      paddingHorizontal: 32,
+      borderRadius: 18,
+    },
+
+    textButton: {
+      color: "#FFF",
+      fontSize: 18,
+    },
+
+    priceText: {
+      color: "#685959",
+      fontSize: 16,
+    },
+
+    price: {
+      fontSize: 20,
       color: '#000000',
       fontWeight: 'bold',
     },
+    
     description: {
       fontSize: 14,
       color: '#000000',
     },
+    
   });
