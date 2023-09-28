d53dbcacd541ef938910b209d550901147064bc2
Ailton Baúque <nevskya@hotmail.com>
2023-09-28 16:32:02 +0200

    fix: making header pass by parameter the title, and fix on derivated files

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,11 +3,11 @@ import {styles} from './styles'
 import {Feather} from '@expo/vector-icons'
 import Ionicons from '@expo/vector-icons/Ionicons';
 
-export function Header() {
+export function Header(props) {
     return(
         <View style={styles.container}>
             <Ionicons name="arrow-back" size={24} color="black" />
-            <Text style={{ flex: 1, textAlign: 'center' }}>My Cart</Text>
+            <Text style={{ flex: 1, textAlign: 'center' }}>{props.title}</Text>
         </View>
 
     )
