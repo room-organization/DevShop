2e1412228a132a7dbdf8f89901fe26de16f898e1
enoquetembe <enoquetembetembe10@gmail.com>
2023-10-03 22:48:58 +0200

    refactor: ajusting sizes, padding at home & update Product page

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -3,30 +3,24 @@ import { styles } from "./styles";
 import { ProductCard } from "../ProductCard";
 import { useEffect, useState } from "react";
 import { api } from "../../lib/axios";
+import { Product } from "../../utils/types";
 
 export function ProductList() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-      async function fetchProducts() {
-        const response = await api.get('/products')
-        setProducts(response.data)
-      }
-  
-      fetchProducts()
-    }, [])
-  return(
-      <View style={styles.container}>
-        {
-          products.map((product) => {
-            return(
-              <ProductCard 
-                key={product.id}
-                data={product}
-              />
-            )
-          })
-        }
-      </View>
-  )
+    async function fetchProducts() {
+      const response = await api.get("/products");
+      setProducts(response.data);
+    }
+
+    fetchProducts();
+  }, []);
+  return (
+    <View style={styles.container}>
+      {products.map((product) => {
+        return <ProductCard key={product.id} data={product} />;
+      })}
+    </View>
+  );
 }
--- src/components/rowCategories/styles.ts
+++ src/components/rowCategories/styles.ts
@@ -1,26 +1,25 @@
-import {Dimensions, StyleSheet, StatusBar} from 'react-native'
-import theme from '../../global/styles/theme';
+import { Dimensions, StyleSheet } from "react-native";
+import theme from "../../global/styles/theme";
 
-const width = Dimensions.get('screen').width;
+const width = Dimensions.get("screen").width;
 
 export const styles = StyleSheet.create({
-    rowCategories:{
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-        marginVertical: 10
-    },
-    itemRowCategory:{
-        height: 30,
-        borderRadius: 16,
-        padding: 10,
-        justifyContent: "center",
-        paddingHorizontal: 15,
-        borderWidth: 1,
-    },
-    selectedCategory:{
-        // backgroundColor: theme.colors_backgrounds.secondary
-    },
-    active: {
-      backgroundColor: theme.colors_backgrounds.secondary,
-    }
+  rowCategories: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginVertical: 10,
+  },
+  itemRowCategory: {
+    height: 30,
+    borderRadius: 16,
+    justifyContent: "center",
+    paddingHorizontal: 15,
+    borderWidth: 1,
+  },
+  selectedCategory: {
+    // backgroundColor: theme.colors_backgrounds.secondary
+  },
+  active: {
+    backgroundColor: theme.colors_backgrounds.secondary,
+  },
 });
