302e3802475003566cee8a80ffb3ad20d8db83ab
Ailton Baúque <nevskya@hotmail.com>
2023-10-05 09:22:50 +0200

    fix: categories on home opening products

--- src/components/infoStatusBar/index.tsx
+++ src/components/infoStatusBar/index.tsx
@@ -1,20 +1,20 @@
 import { View, Image, ImageProps, TouchableOpacity, Text } from 'react-native'
 import { styles } from './styles'
 import { useNavigation } from '@react-navigation/native'
-import { ProductScreenProp } from '../../routers/type.routes'
+import { CategoriesScreenProp } from '../../routers/type.routes'
 interface InfoStatusBarProps {
   src: ImageProps
 }
 
 export const InfoStatusBar = ({ src }: InfoStatusBarProps) => {
-  const navigation = useNavigation<ProductScreenProp>()
+  const navigation = useNavigation<CategoriesScreenProp>()
 
-  function openProduct() {
-    navigation.navigate('Product')
+  function openCategories() {
+    navigation.navigate('Categories')
   }
 
   return (
-    <TouchableOpacity onPress={openProduct}>
+    <TouchableOpacity onPress={openCategories}>
       <View style={styles.container}>
         <Image style={styles.image} source={src} />
       </View>
