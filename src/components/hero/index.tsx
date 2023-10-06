addee0c233459bc8f95f150a53454058656a067d
RodriguesFundo <rodriguesdomingosf@gmail.com>
2023-10-05 22:15:26 -0700

    refeator: update Homwe
    update Hero

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, ImageBackground, TouchableOpacity } from 'react-native'
+import Swiper from 'react-native-swiper'
 
 import { styles } from './styles'
 
 export function Hero() {
-  const [activeElipse, setActiveElipse] = useState(0)
 
-  const handleElipsePress = (index: number) => {
-    setActiveElipse(index)
-  }
 
   const data = [
     require('../../assets/images/hero.jpeg'),
@@ -16,24 +13,17 @@ export function Hero() {
     require('../../assets/images/neom-WLeWJW_WneE-unsplash.jpg'),
   ]
 
+  const slides = data.map((image, index) => (
+    <ImageBackground
+      key={index}
+      source={image}
+      style={styles.imageBackground}
+    />
+  ))
   return (
-    <View style={styles.hero}>
-      <ImageBackground
-        source={data[activeElipse]}
-        style={styles.imageBackground}
-      >
-        <Text style={styles.text}>Explore nossos produtos</Text>
-      </ImageBackground>
-
-      <View style={styles.elipseGroup}>
-        {data.map((image, index) => (
-          <TouchableOpacity
-            key={index}
-            onPress={() => handleElipsePress(index)}
-            style={index === activeElipse ? styles.elipse : styles.elipseOff}
-          ></TouchableOpacity>
-        ))}
-      </View>
-    </View>
+    <Swiper showsButtons={false} autoplay={true} style={styles.hero}>
+      {slides}
+    </Swiper>
+   
   );
 }
--- src/components/hero/styles.ts
+++ src/components/hero/styles.ts
@@ -1,8 +1,5 @@
-import { StyleSheet, Dimensions } from 'react-native'
-import theme from '../../global/styles/theme'
-import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
-
-const height = Dimensions.get('screen').height
+import { StyleSheet} from 'react-native'
+import { RFPercentage } from 'react-native-responsive-fontsize'
 
 export const styles = StyleSheet.create({
   hero: {
@@ -13,28 +10,5 @@ export const styles = StyleSheet.create({
     flex: 1,
     resizeMode: 'cover',
     justifyContent: 'center',
-  },
-  text: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
-  elipseGroup: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginVertical: 10,
-  },
-  elipse: {
-    backgroundColor: theme.colors_backgrounds.btnGreen,
-    width: RFValue(20),
-    height: RFValue(20),
-    borderRadius: RFValue(20),
-    marginHorizontal: RFValue(10),
-  },
-  elipseOff: {
-    backgroundColor: theme.colors_backgrounds.elipseHero,
-    width: RFValue(20),
-    height: RFValue(20),
-    borderRadius: RFValue(20),
-    marginHorizontal: RFValue(10),
   },
 })
