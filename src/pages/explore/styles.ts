2201ad72654614277c3cdb3e6cbd7f68fc104716
RodriguesFundo <rodriguesdomingosf@gmail.com>
2023-10-05 18:00:32 -0700

    feat: Install deep linking dependences

--- src/pages/explore/styles.ts
+++ src/pages/explore/styles.ts
@@ -1,14 +1,36 @@
 import { StyleSheet, StatusBar } from 'react-native'
+import { RFValue } from 'react-native-responsive-fontsize'
 
 export const styles = StyleSheet.create({
   container: {
     paddingTop: StatusBar.currentHeight,
-    flex: 1,
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    borderWidth: 1,
+    flexDirection: 'column'
+  },
+  row:{
+    flexDirection: 'row',
+    gap: 1
+    
+  },
+  info:{
+    flexDirection: 'column',
+    width: RFValue(60),
+    height: RFValue(60),
+    marginVertical: RFValue(2),
+    borderWidth: 1,
+    borderRadius: RFValue(50),
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
+  },
+  icon:{
+    
   },
   text: {
     color: 'black',
     fontSize: 50,
+    // alignSelf: 'flex-start',
   },
 })
