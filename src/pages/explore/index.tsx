971c686cf0ea6dc737892401946137f3fac5b616
RodriguesFundo <rodriguesdomingosf@gmail.com>
2023-10-08 15:03:43 -0700

    feat: dinamic result

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -12,9 +12,12 @@ import {
   IconCategoryItem,
   Title
  } from './styles'
+import { SearchResults } from '../../components/searchResults';
 
 export function Explore() {
   const [search, setSearch] = useState('')  
+  const [searchResult, setSearchResult] = useState('');
+
   useEffect(()=> {
    console.log(search)
  },[search])
@@ -30,57 +33,66 @@ export function Explore() {
           />
         </SearchBar>
       </SearchSection>
-    <Categories>
-      <CategoryItem>
-        <IconCategoryItem name="tshirt"/>
-        <Title>Camisas</Title>
-      </CategoryItem>
-      <CategoryItem>
-        <IconCategoryItem name="shoe-prints"/>
-        <Title>Sapatos Masculinos</Title>
-      </CategoryItem>
-      <CategoryItem>
-        <IconCategoryItem name="tshirt"/>
-        <Title>Roupas Infantis</Title>
-      </CategoryItem>
-      <CategoryItem>
-        <IconCategoryItem name="hat-cowboy"/>
-        <Title>Chapéus</Title>
-      </CategoryItem>
-      <CategoryItem>
-        <IconCategoryItem name="socks"/>
-        <Title>Meias</Title>
-      </CategoryItem>
-      <CategoryItem>
-        <IconCategoryItem name="suitcase"/>
-        <Title>Bolsas</Title>
-      </CategoryItem>
-      <CategoryItem>
-        <IconCategoryItem name="tshirt"/>
-        <Title>Camisas</Title>
-      </CategoryItem>
-      <CategoryItem>
-        <IconCategoryItem name="shoe-prints"/>
-        <Title>Sapatos Masculinos</Title>
-      </CategoryItem>
-      <CategoryItem>
-        <IconCategoryItem name="tshirt"/>
-        <Title>Roupas Infantis</Title>
-      </CategoryItem>
-      <CategoryItem>
-        <IconCategoryItem name="hat-cowboy"/>
-        <Title>Chapéus</Title>
-      </CategoryItem>
-      <CategoryItem>
-        <IconCategoryItem name="socks"/>
-        <Title>Meias</Title>
-      </CategoryItem>
-      <CategoryItem>
-        <IconCategoryItem name="suitcase"/>
-        <Title>Bolsas</Title>
-      </CategoryItem>
       
-    </Categories>
+
+{search ? (
+        // Renderize o componente SearchResults quando houver texto de pesquisa
+        <SearchResults />        
+      ) : (
+        // Renderize as categorias quando não houver texto de pesquisa
+        <Categories>
+          <CategoryItem>
+            <IconCategoryItem name="tshirt"/>
+            <Title>Camisas</Title>
+          </CategoryItem>
+          <CategoryItem>
+            <IconCategoryItem name="shoe-prints"/>
+            <Title>Sapatos Masculinos</Title>
+          </CategoryItem>
+          <CategoryItem>
+            <IconCategoryItem name="tshirt"/>
+            <Title>Roupas Infantis</Title>
+          </CategoryItem>
+          <CategoryItem>
+            <IconCategoryItem name="hat-cowboy"/>
+            <Title>Chapéus</Title>
+          </CategoryItem>
+          <CategoryItem>
+            <IconCategoryItem name="socks"/>
+            <Title>Meias</Title>
+          </CategoryItem>
+          <CategoryItem>
+            <IconCategoryItem name="suitcase"/>
+            <Title>Bolsas</Title>
+          </CategoryItem>
+          <CategoryItem>
+            <IconCategoryItem name="tshirt"/>
+            <Title>Camisas</Title>
+          </CategoryItem>
+          <CategoryItem>
+            <IconCategoryItem name="shoe-prints"/>
+            <Title>Sapatos Masculinos</Title>
+          </CategoryItem>
+          <CategoryItem>
+            <IconCategoryItem name="tshirt"/>
+            <Title>Roupas Infantis</Title>
+          </CategoryItem>
+          <CategoryItem>
+            <IconCategoryItem name="hat-cowboy"/>
+            <Title>Chapéus</Title>
+          </CategoryItem>
+          <CategoryItem>
+            <IconCategoryItem name="socks"/>
+            <Title>Meias</Title>
+          </CategoryItem>
+          <CategoryItem>
+            <IconCategoryItem name="suitcase"/>
+            <Title>Bolsas</Title>
+          </CategoryItem>
+        </Categories>
+      )}
+      
+    
   </Container>
 
   )
