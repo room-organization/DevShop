3cff919f6417bd6882b3bd618adff23fca7a9a63
enoquetembe <enoquetembetembe10@gmail.com>
2023-10-09 19:53:53 +0200

    feat: creating native android and ios app

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, Text, SafeAreaView, Image } from 'react-native'
-import {Feather} from '@expo/vector-icons'
+import { Feather } from '@expo/vector-icons'
 
 import { styles } from './styles'
 import { RFValue } from 'react-native-responsive-fontsize'
@@ -10,21 +10,18 @@ export function Explore() {
     <SafeAreaView style={styles.container}>
       <Text>Man Fashion</Text>
       <View style={styles.row}>
-        <Category/>
+        <Category />
       </View>
       <Text>Woman Fashion</Text>
-      <View style={styles.row}>
-        
-      </View>
+      <View style={styles.row}></View>
     </SafeAreaView>
   )
 }
 
-
 const Category = () => {
-  return(
+  return (
     <View style={styles.info}>
-      <Feather name='cloud-off' size={RFValue(40)} style={styles.icon}/>
+      <Feather name="cloud-off" size={RFValue(40)} style={styles.icon} />
       {/* <Text>Category</Text> */}
     </View>
   )
