dacb451ca45a9803868e8af03c929fc20fd372ac
enoquetembe <enoquetembetembe10@gmail.com>
2023-10-10 18:42:05 +0200

    feat: adding Stripe checkout functionality

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const api = axios.create({
   baseURL:
-    'https://rom-ecommerce-admin.vercel.app/api/84773179-5a70-482b-90f4-df19c9926b09',
+    'https://rom-ecommerce-admin.vercel.app/api/0ab0b5c0-eb98-41fe-b902-a85f4aaf3d15',
 })
